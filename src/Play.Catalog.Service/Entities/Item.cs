1b446003820cd0bf7f661a326ddd33f8034b0ff9
Saurabh09Code <saurabhpathak09041997@gmail.com>
2023-06-29 22:48:29 +0530

    New Changes

--- src/Play.Catalog.Service/Entities/Item.cs
+++ src/Play.Catalog.Service/Entities/Item.cs
@@ -6,13 +6,9 @@ namespace Play.Catalog.Service.Entities
     public class Item : IEntity
     {
         public Guid Id { get; set; }
-
         public string Name { get; set; } = null!;
-
         public string Description { get; set; } = null!;
-
         public decimal Price { get; set; }
-
         public DateTimeOffset CreatedDate { get; set; }
     }
 }
