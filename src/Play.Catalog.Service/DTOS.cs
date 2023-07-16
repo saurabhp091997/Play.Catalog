cab3dba46de36d166ce43351a84cbeb098ba204c
Saurabh09Code <saurabhpathak09041997@gmail.com>
2023-07-16 20:55:43 +0530

    New Changes

--- src/Play.Catalog.Service/DTOS.cs
+++ src/Play.Catalog.Service/DTOS.cs
@@ -3,13 +3,7 @@ using Play.Catalog.Service.Entities;
 
 namespace Play.Catalog.Service.DTOS
 {
-    public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate)
-    {
-        public static implicit operator ItemDto(Item v)
-        {
-            throw new NotImplementedException();
-        }
-    }
+    public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
 
     public record CreateItemDto([Required] string Name, string Description, [Range(0, 1000)] decimal Price);
 
